ee96b3198ad66310433813418ba7a92bd8e172cc
Jorge <jorge.holguin1105@gmail.com>
2023-07-18 17:46:49 -0500

    Aplicacion web terminada

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,13 +1,12 @@
 import './CreateTodoButton.css'
 
-function CreateTodoButton () {
+function CreateTodoButton ({setOpenModal}) {
     return (
     <button 
     className ="CreateTodoButton"
     onClick={
-      (event) => {console.log('le diste clic')
-            console.log(event)
-            console.log(event.target)
+    () => {
+      setOpenModal(state =>!state);
     }
     }>+</button>
     );
