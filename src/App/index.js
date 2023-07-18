f6dde14e68a750a4d66ede3a0f5e91746f3a0d1a
Jorge <jorge.holguin1105@gmail.com>
2023-07-18 01:29:04 -0500

    React Project part 1

---
+++ src/App/index.js
@@ -0,0 +1,84 @@
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
+import React from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+// const defaultTodos = [
+//   {text: 'Cortar cebolla', completed: true },
+//   {text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   {text: 'Llorar con la Llorona', completed: false },
+//   {text: 'LALALALA', completed: false },
+//   {text: 'Usar estados derivados', completed: true},
+// ];
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+
+// localStorage.removeItem('TODOS_V1');
+
+function App() {
+  
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', [] );
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter( todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+
+  console.log ('Log 1');
+  React.useEffect ( () => {console.log ('Log 2')});
+  console.log ('Log 3');
+  const searchedTodos = todos.filter(
+    todo => {
+      const todoText = todo.text.toLocaleLowerCase();
+      const searchText = searchValue.toLowerCase();
+
+      return todoText.includes(searchText);
+    }
+  );
+    
+  const completeTodo = (text) => {
+    const newTodos = [...todos];// copia del estado de los todos
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];// copia del estado de los todos
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  }
+  return (
+    <React.Fragment> 
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch searchValue = {searchValue}
+        setSearchValue = {setSearchValue}
+      />
+        
+      <TodoList>
+        {searchedTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment> 
+  );
+}
+
+
+export default App;
---
+++ src/TodoCounter/index.js
@@ -0,0 +1,14 @@
+// const estilos = {
+//   backgroundColor: 'red'
+// }
+import './TodoCounter.css'
+function TodoCounter ({total , completed}) {
+    return (
+      <h1 className="TodoCounter">
+        Haz completado <span>{completed}</span> 
+        de <span>{total}</span> TODOS
+      </h1>
+    );
+  }
+
+  export {TodoCounter};
